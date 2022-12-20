89e4b0429cd29c62e6d880375e98fe26af0186d2
Monet <shelbyhernandez9876@yahoo.com>
2022-12-20 16:40:19 -0600

    imported media query hooks to app file, imported serif google fonts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
 function App() {
+  const [selectedPage, setselectedPage] = useState("home");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return <div className="App"></div>;
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
+        gray: "#ededed",
+        "deep-blue": "#010026",
+        "dark-gray": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+        "gradient-rainblue":
+          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+      }),
+      fontFamily: {
+        worksans: ["Work Sans", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+      },
+      content: {},
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
