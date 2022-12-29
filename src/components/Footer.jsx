4b7f1e19ff51951c9507a7563295b852892dee49
Monet <shelbyhernandez9876@yahoo.com>
2022-12-28 20:34:23 -0600

    added other component files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
  return (
-    <footer className="md:flex justify-center md:justify-between text-center">
+    <footer className="justify-center text-center">
         <p className="font-playfair text-xl">© 2022 Made with 
         <span class="text-red-500"> ❤ </span> by Shelby Hernandez</p>
     </footer>
