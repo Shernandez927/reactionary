c6916e5082655a7dacdcb9e3107bd7be21e2b1d4
Monet <shelbyhernandez9876@yahoo.com>
2023-01-03 00:46:28 -0600

    installed react router, added respective paths,  added resume pdf

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
@@ -14,23 +20,28 @@ function App() {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return (
-    <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setselectedPage={setselectedPage} />
-      <div className="w-5/6 mx-auto md:h-full">
-        {isAboveMediumScreens && (
-          <DotGroup
-            selectedPage={selectedPage}
-            setselectedPage={setselectedPage}
-          />
-        )}
-        <Home />
-        <Skills />
-        <Projects />
-        <Resume />
-        <Contact />
-        <Footer />
+    <Router>
+      <div className="app bg-deep-blue">
+        <Navbar selectedPage={selectedPage} setselectedPage={setselectedPage} />
+        <div className="w-5/6 mx-auto md:h-full">
+          {isAboveMediumScreens && (
+            <DotGroup
+              selectedPage={selectedPage}
+              setselectedPage={setselectedPage}
+            />
+          )}
+
+          <Routes>
+            <Route path="/" element={[<Home />, <Skills />]} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,51 +10,110 @@ import tailwind from "../assets/images/tailwind.png";
 import react from "../assets/images/react.png";
 
 const Skills = () => {
-return (
+  return (
     <section id="skills" className="pt-10 pb-24">
-         <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-       <p className="font-worksans text-center font-semibold text-4xl mb-5">
-            Techincal <span className="text-rose">Skills</span>
-          </p>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <p className="font-worksans text-center font-semibold text-4xl mb-5">
+          Techincal <span className="text-rose">Skills</span>
+        </p>
         <div className="w-full grid justify-items-center grid-cols-2 md:grid-cols-4 gap-8 text-center py-10 px-12 sm:px-0">
-          <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer">
-            <img src={html} alt="html5" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer">
-            <img src={css} alt="css3" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
-            <img src={js} alt="javascript" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://bulma.io/documentation/" target="_blank" rel="noreferrer">
-            <img src={bulma} alt="bulmacss" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://tailwindcss.com/docs/installation" target="_blank" rel="noreferrer">
-            <img src={tailwind} alt="tailwindcss" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://nodejs.org/en/docs/" target="_blank" rel="noreferrer">
-            <img src={node} alt="nodejs" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://dev.mysql.com/doc/" target="_blank" rel="noreferrer">
-            <img src={sql} alt="mysql" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-            <a href="https://reactjs.org/docs/getting-started.html" target="_blank" rel="noreferrer">
-            <img src={react} alt="reactjs" className="hover:scale-110 ease-in-out duration-500"/>
-            </a>
-
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={html}
+              alt="html5"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://www.w3.org/TR/CSS/#css"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={css}
+              alt="css3"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={js}
+              alt="javascript"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://bulma.io/documentation/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={bulma}
+              alt="bulmacss"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://tailwindcss.com/docs/installation"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={tailwind}
+              alt="tailwindcss"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://nodejs.org/en/docs/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={node}
+              alt="nodejs"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a href="https://dev.mysql.com/doc/" target="_blank" rel="noreferrer">
+            <img
+              src={sql}
+              alt="mysql"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
+          <a
+            href="https://reactjs.org/docs/getting-started.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={react}
+              alt="reactjs"
+              className="hover:scale-110 ease-in-out duration-500"
+            />
+          </a>
         </div>
-        </motion.div>
+      </motion.div>
     </section>
-)
-}
+  );
+};
 
 export default Skills;
