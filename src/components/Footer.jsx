915da7d1e67b1ac26c0aa9510164b1e8731c1da3
Monet <shelbyhernandez9876@yahoo.com>
2023-01-10 00:47:05 -0600

    added social links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,39 @@
-
-
 const Footer = () => {
- return (
+  return (
     <footer className="justify-center text-center pb-8">
-        <p className="font-worksans text-xl">© 2022 Made with 
-        <span className="text-red-500"> ❤ </span> by Shelby Hernandez</p>
+      <p className="font-worksans text-xl">
+        © 2022 Made with
+        <span className="text-red-500"> ❤ </span> by Shelby Hernandez
+      </p>
+      <br></br>
+      <div className="md:flex justify-center md:justify-between text-center text-2xl">
+      <a
+        className="hover:opacity-50 hover:scale-110 transition duration-500 p-2"
+        href="https://github.com/Shernandez927"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a
+        className="hover:opacity-50 hover:scale-110 transition duration-500 p-2"
+        href="https://www.linkedin.com/in/shelby-hernandez-801252220/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+      <a
+        className="hover:opacity-50 hover:scale-110 transition duration-500 p-2"
+        href="https://stackoverflow.com/users/19430732/shernand927"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="fa-brands fa-stack-overflow"></i>
+      </a>
+      </div>
     </footer>
- )
-}
+  );
+};
 
 export default Footer;
